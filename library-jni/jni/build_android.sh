06829ebcc9206729d9e3f073939ab81c057c444e
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2015-07-03 09:11:25 +0200

    Enable logging in build_android.sh

--- library-jni/jni/build_android.sh
+++ library-jni/jni/build_android.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 if [ "$NDK" = "" ]; then
 	echo NDK variable not set, exiting
 	echo "Use: export NDK=/your/path/to/android-ndk"
